4723517065e411fa28dafd0698d1afd4fb594f37
manauser <i@manauser.com>
2023-09-28 16:52:02 +0400

    feat: add header

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,18 +1,18 @@
 import { FC } from 'react'
-import reactLogo from '@assets/images/react.svg'
 import {
   PropsWithEmotionNaming,
   withEmotionNaming,
 } from '@manauser/react-emotion-naming'
 import { withRenderLog } from '@manauser/react-render-log'
 
+import { Header } from '@widgets/header'
 import { Posts } from '@widgets/posts'
 
 import { StyledHome } from './home.style'
 
 const Home: FC<PropsWithEmotionNaming<{}>> = ({ setClassName }) => (
   <StyledHome className={setClassName('Home')}>
-    <img src={reactLogo} alt="" className="fixed right-4 top-4 h-4" />
+    <Header />
     <Posts />
   </StyledHome>
 )
---
+++ src/widgets/header/header.tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react'
+import reactLogo from '@assets/images/react.svg'
+import { withRenderLog } from '@manauser/react-render-log'
+
+import { usePostsFetching } from '@entities/post'
+
+const Header: FC = () => {
+  const { posts, isPostsSuccess } = usePostsFetching()
+
+  return (
+    <header className="flex h-[48px] flex-none items-center justify-between border-b-[1px] border-solid border-blue-300 bg-cyan-950 text-blue-300">
+      <img src={reactLogo} alt="" className="m-4 h-4" />
+      <div className="flex flex-1 items-center justify-center">
+        {isPostsSuccess ? `Posts: ${posts?.length ?? 0}` : 'Loading...'}
+      </div>
+    </header>
+  )
+}
+
+export default withRenderLog(Header)
