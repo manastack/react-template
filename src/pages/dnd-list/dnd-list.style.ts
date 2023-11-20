d56f817205e507cfa5511597bea92e24d4e394dc
manauser <i@manauser.com>
2023-11-20 12:39:22 +0400

    fix: dnd-list markup

--- src/pages/dnd-list/dnd-list.style.ts
+++ src/pages/dnd-list/dnd-list.style.ts
@@ -2,5 +2,5 @@ import styled from '@emotion/styled'
 import tw from 'twin.macro'
 
 export const StyledDndList = styled.div`
-  ${tw`flex flex-auto items-center justify-center`};
+  ${tw`flex flex-auto flex-col items-center justify-start overflow-y-auto p-2`};
 `
--- src/widgets/dnd-item/dnd-item.style.ts
+++ src/widgets/dnd-item/dnd-item.style.ts
@@ -2,5 +2,5 @@ import styled from '@emotion/styled'
 import tw from 'twin.macro'
 
 export const StyledDndItem = styled.div`
-  ${tw`mb-2 w-[200px] cursor-grab rounded-sm bg-blue-500 p-2`};
+  ${tw`mb-2 w-[200px] cursor-grab rounded-sm bg-blue-500 px-4 py-8`};
 `
